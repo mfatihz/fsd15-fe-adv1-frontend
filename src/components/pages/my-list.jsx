3cf156e47aaf73a69e25417976b2627d254df4ec
mfatihz <fatih@reksolindo.co.id>
2025-07-15 14:29:01 +0700

    Update mylist

--- src/components/pages/my-list.jsx
+++ src/components/pages/my-list.jsx
@@ -1,15 +1,13 @@
 import { useEffect, useState } from 'react';
-// import { myList } from "../../utils/data/my-list-page-data"
 import MainPageTemplate from "../templates/main-page-template"
-
-import useLocalStorage from "../../hooks/use-local-storage";
+// import useLocalStorage from "../../hooks/use-local-storage";
 import { getMyListGalleries } from '../../services/api/myList-service';
 
 function MyList() {
   const [userId] = useState('chill_user')
   const [ galleries, setGalleries ] = useState([]);
-  const { toggleId: idToggleHandler, checkId } = useLocalStorage("my-lists", new Set());
-  const { storedValue } = useLocalStorage('my-lists', new Set());
+  // const { toggleId: idToggleHandler, checkId } = useLocalStorage("my-lists", new Set());
+  // const { storedValue } = useLocalStorage('my-lists', new Set());
 
   useEffect(() => {
     const fetchGalleries = async () => {
@@ -22,8 +20,7 @@ function MyList() {
     };
 
     fetchGalleries();
-    console.log(storedValue)
-  }, [userId, storedValue ]);
+  }, [userId ]);
   
   return (
     <MainPageTemplate
